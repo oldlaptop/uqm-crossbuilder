0707d52bcbb4e47cd383689be726aeae15f192fa
oldlaptop <oldlaptop654@aol.com>
2012-10-31 16:38:12 -0400

    Add new shellscripts, includes. System should be ready for initial release.

---
+++ config/includes.chroot/etc/skel/cross-build.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+#
+# This is a helper script for cross-building UQM. Please copy this
+# into any UQM source directory you extract and invoke it as you
+# would build.sh
+
+# envvar CROSS_ROOT
+#
+# The root of our cross environment. This is not used
+# directly by the build system but simplifies definition
+# of our other envvars.
+export CROSS_ROOT=/usr/i686-w64-mingw32
+
+# envvar BUILD_HOST
+#
+# Tells the UQM build system that it is crosscompiling with
+# mingw32.
+export BUILD_HOST=MINGW32
+
+# envvar PKG_CONFIG_PATH
+#
+# Tells the UQM build system where to find the pkgconfig files
+# for our cross-toolchain
+export PKG_CONFIG_PATH=$CROSS_ROOT/lib/pkgconfig
+
+# We need these extra LDFLAGS so UQM will compile properly against
+# libsdl (and find libsdl-image).
+export LDFLAGS="-lmingw32 -lSDLmain"
+
+# Uncomment this line to turn on HIGHLY verbose debug information
+# (you will want to redirect it to a file with something like
+# ./cross-build.sh uqm 2> build.log
+#export SH="sh -x"
+
+# UQM does not use autoconf and therefore does not know about
+# autoconf host triplets. To fix this we have created symlinks
+# in ${CROSS_ROOT}/build to the appropriate tools and will set
+# PATH here so they override any native tools present.
+export PATH=$CROSS_ROOT/bin:$PATH
+
+# Meant to keep the envvars from being tampered with by other shells
+# (according to UQM documentation)
+unset ENV BASH_ENV
+
+./build.sh $@
---
+++ config/includes.chroot/usr/bin/build-uqm-dependencies.sh
@@ -0,0 +1,65 @@
+#! /bin/sh
+
+# Takes care of cross-building the different UQM depencencies.
+#
+# This script is merely a wrapper, it specifies envvars for the scripts that do
+# the actual work, then launches them. Only this script should be called directly.
+
+# Uncomment the following line to keep build directories intact.
+export KEEP_TEMP="1"
+
+# envvar HOST_TRIPLET
+#
+# This is used as the host triplet for the cross toolchain.
+# Autoconf-using packages will get this in their --host option,
+# and packages will be installed to /usr/$HOST_TRIPLET.
+# Do not change unless you have changed the cross toolchain to be used
+# in base-cross-toolchain.list.
+export HOST_TRIPLET="i686-w64-mingw32"
+
+# envvar LIBICONV_URL
+#
+# Specifies the URL of the libiconv source tarball you want to use.
+export LIBICONV_URL="ftp://ftp.wayne.edu/pub/gnu/libiconv/libiconv-1.14.tar.gz"
+
+# envvar ZLIB_URL
+#
+# Specifies the URL of the zlib source tarball you want to use.
+export ZLIB_URL="http://zlib.net/zlib-1.2.7.tar.gz"
+
+# envvar LIBOGG_URL
+#
+# Specifies the URL of the libogg source tarball you want to use.
+export LIBOGG_URL="http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
+
+# envvar LIBVORBIS_URL
+#
+# Specifies the URL of the libvorbis source tarball you want to use.
+export LIBVORBIS_URL="http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz"
+
+# envvar LIBSDL_URL
+#
+# Specifies the URL of the libsdl source tarball you want to use.
+export LIBSDL_URL="http://www.libsdl.org/release/SDL-1.2.15.tar.gz"
+
+# envvar LIBSDL_IMAGE_URL
+#
+# Specifies the URL of the libsdl_image source tarball you want to use.
+export LIBSDL_IMAGE_URL="http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
+
+echo "Building UQM dependencies"
+echo ""
+
+. /usr/src/crossbuild/crossbuild_libiconv
+. /usr/src/crossbuild/crossbuild_zlib
+. /usr/src/crossbuild/crossbuild_libogg
+. /usr/src/crossbuild_libvorbis
+. /usr/src/crossbuild_libsdl
+. /usr/src/crossbuild_libsdl-image
+
+echo ""
+echo "Finished building UQM dependencies"
+
+if [ ! ${KEEP_TEMP} ]; then
+	rm /tmp/*
+fi
