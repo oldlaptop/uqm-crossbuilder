bbca4429db78db3139905da61bfc7d345ba930ca
oldlaptop <oldlaptop654@aol.com>
2013-06-12 15:08:27 -0400

    "Finish" dependency resolver, it should actually resolve dependencies now

--- config/includes.chroot/usr/src/crossbuild/depengine.py
+++ config/includes.chroot/usr/src/crossbuild/depengine.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 
 import os
 import glob
@@ -40,10 +40,63 @@ def build_depsdict (pkgdict):
 					continue
 	return depsdict
 
+def build_install_list (pkgdict, depsdict, wanted_list):
+	'''
+	Builds the list of packages to be installed, based on dependencies of package names
+	found in wanted_list. Sorting is done in terms of installation order; packages will
+	be placed after all their dependencies.
+	'''
+	
+	# First we will determine a list of all packages to be installed, pruning duplicates
+	# (dependencies required by multiple packages, dependencies already specified in
+	# wanted_list, etc. We first make a duplicate-less copy of wanted_list, then iterate
+	# repeatedly through all members in the copy, adding their dependencies to the copy
+	# if they are not already present, until the loop runs once without altering the copy.
+	# (We unfortunately cannot do the elegant thing and use comprehensions for this, since
+	# the loop must reference the structure it's modifying, and doing this from inside a
+	# comprehension is horribly ugly, defeating the purpose. Python also really should have
+	# a do ... while analog, as it is we need an ugly hack for that as well...)
+	
+	# First make the pruned copy
+	install_list = []
+	for pkgname in wanted_list:
+		if pkgname not in install_list:
+			install_list.append (pkgname)
+	# Now add dependencies
+	while True:
+		modified_this_run = False
+		for pkgname in install_list:
+			for candidate in depsdict[pkgname]:
+				if candidate not in install_list:
+					install_list.insert (0, candidate)
+					modified_this_run = True
+				else:
+					# If the package comes before its dependency in the installation
+					# order, we need to fix this. (Note that we rely upon the previous
+					# measures to ensure there are no duplicates in the list, a bug which
+					# causes duplicates to be present at this point could cause another
+					# bug here...
+					if install_list.index (pkgname) < install_list.index (candidate):
+						install_list.pop (install_list.index (candidate))
+						install_list.insert (install_list.index (pkgname), candidate)
+						modified_this_run = True
+		if modified_this_run == False:
+			break
+	return install_list
 
 if __name__ == '__main__':
-	script_path = "."
+	import argparse
+	
+	parser = argparse.ArgumentParser (description='Print to stdio a \'\\n\'-delimited list of script files to be run in order to crossbuild the given packages')
+	parser.add_argument ('packages', metavar='package', nargs='+', help='A package you want given in the list')
+	args = parser.parse_args ()
+	
 	pkgdict = build_pkgdict ()
 	depsdict = build_depsdict (pkgdict)
-	print(pkgdict)
-	print(depsdict)
+	install_list = build_install_list (pkgdict, depsdict, args.packages)
+	
+	for pkgname in install_list:
+		print (pkgdict[pkgname])
+	#print (pkgdict)
+	#print (depsdict)
+	#print (install_list)
