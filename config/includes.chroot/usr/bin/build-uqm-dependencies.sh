a062fc49dc918278ae08a747389537546491b3e3
oldlaptop <oldlaptop654@aol.com>
2013-06-29 10:00:14 -0400

    Clean up build-uqm-dependencies.chroot a little
     * Remove scary !!! WARNING !!! about the dependency resolver
    
     * Make sure the dependency resolver worked, and abort if it didn't.

--- config/includes.chroot/usr/bin/build-uqm-dependencies.sh
+++ config/includes.chroot/usr/bin/build-uqm-dependencies.sh
@@ -5,9 +5,6 @@
 # This script is merely a wrapper, it specifies envvars for the scripts that do
 # the actual work, then launches them. Only this script should be called directly.
 
-# Uncomment the following line to keep build directories intact.
-export KEEP_TEMP="1"
-
 # envvar HOST_TRIPLET
 #
 # This is used as the host triplet for the cross toolchain.
@@ -17,49 +14,30 @@ export KEEP_TEMP="1"
 # in base-cross-toolchain.list.
 export HOST_TRIPLET="i686-w64-mingw32"
 
-# envvar LIBICONV_URL
-#
-# Specifies the URL of the libiconv source tarball you want to use.
-export LIBICONV_URL="ftp://ftp.wayne.edu/pub/gnu/libiconv/libiconv-1.14.tar.gz"
-
-# envvar ZLIB_URL
-#
-# Specifies the URL of the zlib source tarball you want to use.
-export ZLIB_URL="http://zlib.net/zlib-1.2.7.tar.gz"
-
-# envvar LIBOGG_URL
-#
-# Specifies the URL of the libogg source tarball you want to use.
-export LIBOGG_URL="http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
-
-# envvar LIBVORBIS_URL
-#
-# Specifies the URL of the libvorbis source tarball you want to use.
-export LIBVORBIS_URL="http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz"
-
-# envvar LIBSDL_URL
-#
-# Specifies the URL of the libsdl source tarball you want to use.
-export LIBSDL_URL="http://www.libsdl.org/release/SDL-1.2.15.tar.gz"
-
-# envvar LIBSDL_IMAGE_URL
-#
-# Specifies the URL of the libsdl_image source tarball you want to use.
-export LIBSDL_IMAGE_URL="http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
-
-echo "Building UQM dependencies"
+echo "Crossbuilding build depenencies"
 echo ""
 
-. /usr/src/crossbuild/crossbuild_libiconv
-. /usr/src/crossbuild/crossbuild_zlib
-. /usr/src/crossbuild/crossbuild_libogg
-. /usr/src/crossbuild_libvorbis
-. /usr/src/crossbuild_libsdl
-. /usr/src/crossbuild_libsdl-image
+echo "Resolving dependencies..."
+echo ""
+/usr/src/crossbuild/depengine.py libsdl libsdl-image libogg libvorbis zlib > /tmp/script_list
+
+if [ ! -f /tmp/script_list ] then;
+	echo "/tmp/script_list not found - dependency resolution failed"
+	exit 1
+fi
+
+# We now iterate over the lines in the output file from our dependency resolver, sourcing each
+# script listed in order.
+export LINES=`cat /tmp/script_list | wc -l`
+for LINE_NUMBER in `seq $LINES`
+do
+	. `head /tmp/script_list -n$LINE_NUMBER | tail -n1`
+done
 
 echo ""
 echo "Finished building UQM dependencies"
 
-if [ ! ${KEEP_TEMP} ]; then
-	rm /tmp/*
-fi
+echo ""
+echo "Perpetrating acts of horrific kludgery against the toolchain"
+
+. /usr/src/crossbuild/toolchain-ugly-hacks
