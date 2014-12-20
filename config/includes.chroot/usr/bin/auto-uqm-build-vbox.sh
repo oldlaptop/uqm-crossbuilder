ea742fa48b515298518ae8cecc268773a4a726a5
oldlaptop <oldlaptop654@aol.com>
2014-12-19 20:57:37 -0500

    slight de-evilization

--- config/includes.chroot/usr/bin/auto-uqm-build-vbox.sh
+++ config/includes.chroot/usr/bin/auto-uqm-build-vbox.sh
@@ -4,11 +4,8 @@
 # 
 # Paramaters:
 #
-# vanilla
-#	Build vanilla
-#
-# balance
-#	Build balance mod from git
+#	1: 'vanilla' to build vanilla, 'balance' to build balance
+#	2: If building balance, the git revision to check out (legal to git-checkout)
 
 # envvar: BALANCE_GIT_URL
 #
@@ -21,66 +18,78 @@ BALANCE_GIT_URL="git://github.com/Roisack/Shiver-Balance-Mod.git"
 # The name for our balance mod effects pack.
 EFFECTS_PACK_NAME="improved-netmelee-effects.zip"
 
-if [ `echo $@ | grep "vanilla"` ]; then
-	mkdir /tmp/uqm
-	tar -zxf /usr/src/uqm/uqm*.tgz -C /tmp/uqm
-	cd /tmp/uqm/uqm-0.7.0
-	cp /etc/skel/cross-build.sh .
-	cp /usr/src/uqm/config.state.vanilla ./config.state
-	echo "Configuring vanilla"
-	# TODO: Find some way of doing this that isn't a horrible, horrible hack.
-	yes '
-	' | sh cross-build.sh uqm config
+# envvar: SHARED_FOLDER
+#
+# Path to the VirtualBox shared folder
+SHARED_DIR="/vbox_share"
 
-	echo "Building vanilla"
-	sh cross-build.sh uqm
+case ${1} in
+	vanilla)
+		mkdir /tmp/uqm
+		tar -zxf /usr/src/uqm/uqm*.tgz -C /tmp/uqm
+		cd /tmp/uqm/uqm-0.7.0
+		cp /etc/skel/cross-build.sh .
+		cp /usr/src/uqm/config.state.vanilla ./config.state
 
-	if [ -f *.exe ]; then
-		cp *.exe /vbox_share
-	else
-		echo "No executables found!"
+		echo "Configuring vanilla"
+		# TODO: Find some way of doing this that isn't a horrible, horrible hack.
+		yes '
+		' | sh cross-build.sh uqm config
+
+		echo "Building vanilla"
+		sh cross-build.sh uqm
+
+		if [ -f *.exe ]
+		then
+			cp *.exe ${SHARED_DIR}
+		else
+			echo "No executables found!"
+			exit 1
+		fi
+		;;
+	balance)
+		cd /tmp
+		git clone $BALANCE_GIT_URL
+		cd Shiver-Balance-Mod
+		cp /etc/skel/cross-build.sh .
+
+		git checkout master
+		cp /usr/src/uqm/config.state.balance ./config.state
+		echo "Configuring balance"
+
+		# Make sure embedded icons are set up
+
+		# TODO: make this more robust
+		sed -i 's/.o)/.o) uqm.res/' Makefile.build
+
+		i686-w64-mingw32-windres src/res/UrQuanMasters.rc -O coff -o uqm.res
+
+		# TODO: Find some way of doing this that isn't a horrible, horrible hack.
+		yes '
+		' | sh cross-build.sh uqm config
+
+		echo "Building balance"
+		sh cross-build.sh uqm
+
+		echo "Building effects pack"
+		sh generate-effects-pack.sh
+
+		if [ -f *.exe ]; then
+			cp *.exe /vbox_share
+		else
+			echo "No executables found!"
+			exit 1
+		fi
+
+		if [ -f content/addons/$EFFECTS_PACK_NAME ]; then
+			cp content/addons/$EFFECTS_PACK_NAME /vbox_share
+		else
+			echo "Effects pack not found!"
+			exit 1
+		fi
+		;;
+	*)
+		echo "Invalid UQM flavor; choose from vanilla or balance"
 		exit 1
-	fi
-fi
-
-if [ `echo $@ | grep "balance"` ]; then
-	cd /tmp
-	git clone $BALANCE_GIT_URL
-	cd Shiver-Balance-Mod
-	cp /etc/skel/cross-build.sh .
-
-	git checkout master
-	cp /usr/src/uqm/config.state.balance ./config.state
-	echo "Configuring balance"
-
-	# Make sure embedded icons are set up
-
-	# TODO: make this more robust
-	sed -i 's/.o)/.o) uqm.res/' Makefile.build
-
-	i686-w64-mingw32-windres src/res/UrQuanMasters.rc -O coff -o uqm.res
-
-	# TODO: Find some way of doing this that isn't a horrible, horrible hack.
-	yes '
-	' | sh cross-build.sh uqm config
-
-	echo "Building balance"
-	sh cross-build.sh uqm
-	
-	echo "Building effects pack"
-	sh generate-effects-pack.sh
-
-	if [ -f *.exe ]; then
-		cp *.exe /vbox_share
-	else
-		echo "No executables found!"
-		exit 1
-	fi
-
-	if [ -f content/addons/$EFFECTS_PACK_NAME ]; then
-		cp content/addons/$EFFECTS_PACK_NAME /vbox_share
-	else
-		echo "Effects pack not found!"
-		exit 1
-	fi
-fi
+		;;
+esac
